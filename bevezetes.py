46d112e2c9a49d6dd7b8683274540879c432b0eb
Németh Áron <nemetharon@fightman01bot.hu>
2022-01-27 19:51:57 +0100

    random kép lekérés

--- bevezetes.py
+++ bevezetes.py
@@ -1,3 +1,4 @@
+import requests
 print("Szia, én vagyok a bevezető programm.")
 print("Ezt a programot szeretnél használni?")
 print("1. Igen")
@@ -19,3 +20,15 @@ while x < 10:
 
 y = "alma"
 print(f"Ez egy {y}")
+
+print("Szeretnél egy kis képet?")
+kep = input("Igen/Nem: ")
+if kep == "Igen":
+    url = "https://picsum.photos/200/300"
+    response = requests.get(url)
+    with open("kep.jpg", "wb") as f:
+        f.write(response.content)
+elif kep == "Nem":
+    print("Köszönöm, hogy nem szeretnél képet.")
+else:
+    print("Érvénytelen válasz.")
